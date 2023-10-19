b2c7d85127f2ffca0b36db56f413b2c0b94ab42a
Vruti Patel <vrutip27@gmail.com>
2023-10-19 20:27:31 +0000

    initial commit

---
+++ questions.js
@@ -0,0 +1,12 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What company was originally called "Cadabra"?",
+    answer: "Amazon",
+    options: [
+      "Amazon",
+      "Walmart",
+      "Ali Express",
+      "Ebay"
+    ]
+  },
